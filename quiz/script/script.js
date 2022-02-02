8ace11c9b3b8b6fca734a3a4964bf943d3e57f64
Mivisha <90041857+Mivisha@users.noreply.github.com>
2022-02-02 22:49:50 +0200

    6.3

--- quiz/script/script.js
+++ quiz/script/script.js
@@ -1,77 +1,136 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
+
     const btnOpenModal = document.querySelector('#btnOpenModal');
     const modalBlock = document.querySelector('#modalBlock');
     const closeModal = document.querySelector('#closeModal');
-    const questonTitle = document.querySelector('#question');
-    const formAnswers = document.querySelector('#formAnswers');
+    const questionTitle = document.querySelector('#question');
+    const formAnswers = document.querySelector('#formAnswers'); 
     const nextButton = document.querySelector('#next');
     const prevButton = document.querySelector('#prev');
+    const sendButton = document.querySelector('#send');
 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
         playTest();
     });
-    
     closeModal.addEventListener('click', () => {
         modalBlock.classList.remove('d-block');
     });
 
     const playTest = () => {
+
+        const finalAnswers = [];
         let numberQuestion = 0;
 
         const renderAnswers = (index) => {
-            questions[index].answers.forEach(answer =>{
+            questions[index].answers.forEach(answer => {
                 const answerItem = document.createElement('div');
 
-                if(numberQuestion === 0) {
-                    prevButton.style.display = "none";
-                }
-                else {
-                    prevButton.style.display = "block";
-                }
+                answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center');
 
-                if(numberQuestion === questions.length-1) {
-                    nextButton.style.display = "none";
-                }
-                else {
-                    nextButton.style.display = "block";
-                }
-                
-                answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
-
-                answerItem.innerHTML = ` 
-                <div class="answers-item d-flex flex-column">
-                    <input type="${questions[index].type}" id="{$answer.title}" name="answer" class="d-none">
-                    <label for="{$answer.title}" class="d-flex flex-column justify-content-between">
-                    <img class="answerImg" src="${answer.url}" alt="burger">
-                    <span>${answer.title}</span>
-                    </label>
-                </div>
+                answerItem.innerHTML = `
+                    <div class="answers-item d-flex flex-column ">
+                        <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
+                        <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                        <img class="answerImg" src="${answer.url}" alt="burger">
+                        <span>${answer.title}</span>
+                        </label>
+                    </div>
                 `;
-
                 formAnswers.appendChild(answerItem);
             });
-        }  
-
-        const renderQuestions = (indexQuestion) =>{;
+        }
+        const renderQuestions = (indexQuestion) => {
             formAnswers.innerHTML = ``;
 
-            questonTitle.innerHTML = `${questions[indexQuestion].question}`;
+            switch (true) {
+                case numberQuestion === 0:
+                    prevButton.classList.add('d-none');
+                    break;
+                case numberQuestion <= questions.length - 1:
+                    prevButton.classList.remove('d-none');
+                    nextButton.classList.remove('d-none');
+                    sendButton.classList.add('d-none');
+                    break;
+                case numberQuestion === questions.length:
+                    prevButton.classList.add('d-none');
+                    nextButton.classList.add('d-none');
+                    sendButton.classList.remove('d-none');
+                    break;
+                default:
+                    break;
+            }
 
-            renderAnswers(indexQuestion);
+            if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                questionTitle.innerHTML = `${questions[indexQuestion].question}`;
+                renderAnswers(indexQuestion);
+            }
+
+            // if (numberQuestion === 0) {
+            //     prevButton.classList.add('d-none');
+            // }
+
+
+            if (numberQuestion === questions.length) {
+                nextButton.classList.add('d-none');
+                prevButton.classList.add('d-none');
+                sendButton.classList.remove('d-none');
+                formAnswers.innerHTML = `
+                    <div class="mb-3">
+                        <label for="numberPhone">Enter your number</label>
+                        <input type="number" class="form-control" id="numberPhone">
+                    </div>
+                `;
+            }
+
+            if (numberQuestion === questions.length + 1) {
+                formAnswers.textContent = `Спасибо за пройденый тест!`;
+                setTimeout(() => {
+                    modalBlock.classList.remove('d-block');
+                }, 2000)
+            }
+
+
         }
 
         renderQuestions(numberQuestion);
 
+        const checkAnswer = () => {
+            const obj = {};
+
+            const inputs = [...formAnswers.elements].filter(input => input.checked || input.id === 'numberPhone');
+
+            inputs.forEach((input, index) => {
+                if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+                    obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+                }
+
+                if (numberQuestion === questions.length) {
+                    obj['Номер телефона'] = input.value;
+                }
+            });
+
+            console.log(obj);
+
+            finalAnswers.push(obj);
+            console.log(" ~ file: script.js ~ line 81 ~ checkAnswer ~ finalAnswers", finalAnswers)
+        }
+
         nextButton.onclick = () => {
+            checkAnswer();
             numberQuestion++;
             renderQuestions(numberQuestion);
         }
-
         prevButton.onclick = () => {
             numberQuestion--;
             renderQuestions(numberQuestion);
         }
-    }    
-})
 
+        sendButton.onclick = () => {
+            checkAnswer();
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+        }
+
+    }
+})
